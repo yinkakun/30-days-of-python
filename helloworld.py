8e1a603a048f5c33066c5935058000c5c5e90b6d
yinkakun <yinkakun@gmail.com>
2023-02-03 00:49:35 +0100

    day one

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,27 @@
 print("Hello World")
+
+print("addition", 3 + 4)
+
+print("subtraction", 3 - 4)
+
+print("multiplication", 3 * 4)
+
+print("modulo", 3 % 4)
+
+print("division", 3 / 4)
+
+print("floor division", 3 // 4)
+
+print("exponent", 3 ** 4)
+
+print("type of 10", type(10))
+
+print("type of 3.14", type(3.14))
+
+print("type of 4 - 4j", type(4 - 4j))
+
+print("type of 'hello world'", type("hello world"))
+
+print("type of True", type(True))
+
+print("type of ['hello', 'world']", type(["hello", "world"]))
